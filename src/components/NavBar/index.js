578edf86356cd82c42a6bd3ec8a36120161680f6
lvtdien2001 <lvtdien.ag@gmail.com>
2023-09-04 21:03:16 +0700

    buiding nav bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,46 @@
 import { useContext } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, Row, Col } from 'react-bootstrap';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import { GiExitDoor } from 'react-icons/gi';
 import { AuthContext } from '../../contexts';
+import styles from './NavBar.module.scss';
+
+const cx = classNames.bind(styles);
 
 const NavBar = () => {
     const { logout } = useContext(AuthContext);
     return (
-        <div>
-            <Button onClick={logout}>Logout</Button>
-        </div>
+        <Row className={`${cx('nav')} justify-content-around`}>
+            <Col xs={7} lg={6}>
+                <Dropdown>
+                    <Dropdown.Toggle variant="primary">
+                        Quản lý
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Link className={cx('text-link')} to='/product'>
+                            <Dropdown.Item as={'div'}>Quản lý sản phẩm</Dropdown.Item>
+                        </Link>
+                        <Link className={cx('text-link')} to='/order'>
+                            <Dropdown.Item as={'div'}>Quản lý đơn hàng</Dropdown.Item>
+                        </Link>
+                        <Link className={cx('text-link')} to='/warehouse'>
+                            <Dropdown.Item as={'div'}>Quản lý kho hàng</Dropdown.Item>
+                        </Link>
+                        <Link className={cx('text-link')} to='/user'>
+                            <Dropdown.Item as={'div'}>Quản lý người dùng</Dropdown.Item>
+                        </Link>
+                        <Link className={cx('text-link')} to='/sales'>
+                            <Dropdown.Item as={'div'}>Thống kê doanh thu</Dropdown.Item>
+                        </Link>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Col>
+            <Col className='text-end' xs={5} lg={2}>
+                <Button className={cx('logout-btn')} onClick={logout}><GiExitDoor /> Đăng xuất</Button>
+            </Col>
+        </Row>
     )
 }
 
